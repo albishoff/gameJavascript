3102b4d8ab7cb339939e9a9d1b41d12b2cbff5b8
Unknown <acharne@tut.by>
2019-03-17 19:07:23 +0300

    create rectangles

---
+++ create_rectangle/js/app.js
@@ -0,0 +1,35 @@
+let $start = document.querySelector('#start');
+let $game = document.querySelector('#game');
+
+let score = 0;
+
+$start.addEventListener('click', () => {
+	console.log('start');
+	$start.classList.add('hide');
+
+	$game.style.backgroundColor = '#fff';
+
+	renderBox();
+});
+
+$game.addEventListener('click', event => {
+	if (event.target.dataset.box) {
+		score++;
+		renderBox();
+	}
+});
+
+function renderBox() {
+	$game.innerHTML = '';
+	let box = document.createElement('div');
+
+	box.style.height = box.style.width = '50px';
+	box.style.position = 'absolute';
+	box.style.backgroundColor = '#000';
+	box.style.top = '50px';
+	box.style.left = '70px';
+	box.style.cursor = 'pointer';
+	box.setAttribute('data-box', 'true');
+
+	$game.insertAdjacentElement('afterbegin', box);
+}
